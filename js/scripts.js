b5e3a56265b3047fb94447cd593d2de13d8ea2ea
Randy Leighton & Guy Halperin <student@epicodus.com>
2014-07-15 15:53:12 -0700

    Add JS implementation

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,48 @@
 $(document).ready(function () {
-  $("form#task").submit(function(event) {
-    var inputTask = $("input#new-task").val();
 
-    var newTask = {taskToDo: inputTask, completed: false};
+  $("form#task").submit(function(event) {
+    var inputList = $("input#new-list").val();
+    var lists = [];
+    lists.push(inputList);
+    var newList = {name: inputList, tasks: []}
+    //newList.listType.push({list: inputList})
     $("#second-column").show();
 
-    $("#task-list").append("<div id=" + newTask.taskToDo + " class='checkbox'><input type='checkbox' value=''>"
-          + newTask.taskToDo + "</div");
+    $("#list-types").append("<div class='list'>" + newList.name + "</div>");
 
     $("#task").find("input").val(""); // this line clears the input boxes
 
-    $(".checkbox input").last().click(function (){
-      if(!newTask.completed){
-        $(this).parent().css("text-decoration", "line-through");
-        newTask.completed = true;
-      }else{
-        $(this).parent().css("text-decoration", "none");
-        newTask.completed = false;
-      }
+    $(".list").last().click(function(){
+      $("#third-column").show();
+      $("#list-title").text(newList.name);
+      $("#list-items").empty();
 
-      // $(this).parent().css("text-decoration", "line-through");
+      newList.tasks.forEach(function(task) {
+        $("#list-items").append("<div>" + task.description + "</div>");
+      });
+      // event.preventDefault();
+
+      $("form#list-task").unbind('submit').submit(function(event) {
+        var inputTask = $("input#list-task").val();
+        var newTask = {description: inputTask};
+        newList.tasks.push(newTask);
+        $("#list-items").append("<div>" + newTask.description + "</div>");
+        console.log(newList.tasks)
+
+        event.preventDefault();
+      });
+
     });
+  event.preventDefault();
+  });
 
+  $("form#list-task").submit(function(event) {
+    var inputTask = $("input#list-task").val();
+    var newTask = {task: []};
+    $("#list-items").append("<div>" + newTask.task + "</div>");
 
   event.preventDefault();
   });
+
 
 });
